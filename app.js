afd6dd06d1f4f72605f362fbc57919aae6245457
Santiago <dellasala1992@gmail.com>
2023-11-06 11:49:40 -0300

    Santiago_ corrección app.js

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ const PORT = 3030;
 app.use(express.static(path.join(__dirname,'public')));
 
 /* rutas */
-app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','index.html')))
-app.get('/register',(req,res) => res.sendFile(path.join(__dirname,'views','register.html')))
-app.get('/login',(req,res) => res.sendFile(path.join(__dirname,'views','login.html')))
-app.get('/product-detail',(req,res) => res.sendFile(path.join(__dirname,'views','productDetail.html')))
-app.get('/product-cart',(req,res) => res.sendFile(path.join(__dirname,'views','productCart.html')))
+app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','index.html')));
+app.get('/register',(req,res) => res.sendFile(path.join(__dirname,'views','register.html')));
+app.get('/login',(req,res) => res.sendFile(path.join(__dirname,'views','login.html')));
+app.get('/product-detail',(req,res) => res.sendFile(path.join(__dirname,'views','productDetail.html')));
+app.get('/product-cart',(req,res) => res.sendFile(path.join(__dirname,'views','productCart.html')));
 
 
 
-app.listen(PORT, () => console.log(`Server running in http://localhost:${PORT}`))
+app.listen(PORT, () => console.log(`Server running in http://localhost:${PORT}`));
