366280ad2b252d4b78c36e6144ff8426eb967e2d
YDLucero <yohanna.lucero@gmail.com>
2023-11-16 20:16:34 -0300

    det.producto

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.static(path.join(__dirname,'public')));
 app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','index.html')));
 app.get('/register',(req,res) => res.sendFile(path.join(__dirname,'views','register.html')));
 app.get('/login',(req,res) => res.sendFile(path.join(__dirname,'views','login.html')));
-app.get('/product-detail',(req,res) => res.sendFile(path.join(__dirname,'views','productDetail.html')));
+app.get('/productDetail',(req,res) => res.sendFile(path.join(__dirname,'views','productDetail.html')));
 app.get('/product-cart',(req,res) => res.sendFile(path.join(__dirname,'views','productCart.html')));
 
 
