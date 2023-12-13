8b10572ba497382296c82ffa1c5f6208a6bb13e6
YDLucero <yohanna.lucero@gmail.com>
2023-12-13 02:24:24 -0300

    vista admin-dashboard y product-edit

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { detail, add } = require('../controllers/productsController');
+const { detail, add, edit } = require('../controllers/productsController');
 
 /* productos */
 router
   .get('/detail', detail)
   .get('/agregar',add)
+  .get('/editar',edit)
 
 module.exports = router
