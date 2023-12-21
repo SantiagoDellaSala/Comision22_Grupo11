73da28dcb673db1d204588325c90fa712a01b91c
YDLucero <yohanna.lucero@gmail.com>
2023-12-21 10:38:06 -0300

    index.js

--- src/data/index.js
+++ src/data/index.js
@@ -2,6 +2,6 @@ const {readFileSync} = require('fs')
 
 module.exports = {
     leerJSON : (filename) => {
-        return JSON.parse(readFileSync(`./src/data/${filename}.json`,'utf-8'))
+        return JSON.parse(readFileSync(`./data/${filename}.json`,'utf-8'))
     }
 }
