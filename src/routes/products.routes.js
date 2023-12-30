76cdb571a9b3de13caae164b0eafb2b0d05a3e8d
Santiago <dellasala1992@gmail.com>
2023-12-30 12:10:06 -0300

    Santiago_Creando vista de todos los productos

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { detail, add, edit } = require('../controllers/productsController');
+const { detail, add, edit, productos } = require('../controllers/productsController');
 
 /* productos */
 router
