8926f755ffe7838fcd29d49d09d2eea4831cb8a8
YDLucero <yohanna.lucero@gmail.com>
2024-01-11 09:24:35 -0300

    ./src en products.js

--- src/data/index.js
+++ src/data/index.js
@@ -2,10 +2,10 @@ const {readFileSync, writeFileSync} = require('fs')
 
 module.exports = {
     leerJSON : (filename) => {
-        return JSON.parse(readFileSync(`./data/${filename}.json`,'utf-8'))
+        return JSON.parse(readFileSync(`./src/data/${filename}.json`,'utf-8'))
     },
    escribirJSON: (data, filename)=>{
-    writeFileSync(`./data/${filename}.json`, JSON.stringify(data,null,3), 'utf-8')
+    writeFileSync(`./src/data/${filename}.json`, JSON.stringify(data,null,3), 'utf-8')
     return null
    } 
 }
