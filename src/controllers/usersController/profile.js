65c2d707aea295a4efa12a7f854c5aaf33c6e13d
YDLucero <yohanna.lucero@gmail.com>
2024-02-29 00:06:47 -0300

    deleted folders: profileEdit, profileUpload

--- src/controllers/usersController/profile.js
+++
@@ -1,9 +0,0 @@
-  /* SANTIAGO */
-  module.exports=
-  profile= (req, res) => {
-    // const users = leerJSON("users");
-    // const user = users.find((user) => user.id === req.params.id);
-    return res.render("users/profile"
-    // , {user,}
-    );
-  }
--- src/controllers/usersController/profileEdit.js
+++
@@ -1,9 +0,0 @@
-module.exports =
-
-profileEdit = (req, res) => {
-    // const user = users.find((user) => user.id === req.params.id);
-
-    return res.render("users/profile-edit"
-    // , {...user,}
-    );
-  }
