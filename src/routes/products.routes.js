30f6b9e2e5ed2f053265e4ac8dcb562a8ce56651
Ulises Farias <Ulisesfarias1905@hotmail.com>
2024-04-10 10:23:29 -0300

    Pull develop

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer')
 const path = require('path')
-const productAddValidator = require('../validations/product-add-validator')
 const { detail, add, edit, update, create, allProducts, remove, filterCat} = require('../controllers/productsController');
 
 const productAddValidator = require('../validations/product-add-validator')
@@ -24,15 +23,10 @@ const upload = multer({storage})
 router
   .get('/allProducts', allProducts)
   .get('/detail/:id', detail)
-  .get('/agregar',productAddValidator,add)
+  .get('/agregar', add)
   .get('/editar/:id', edit)
-<<<<<<< HEAD
   .put('/editar/:id',upload.single('mainImage'),productEditValidator, update)
   .post('/create',upload.single('mainImage'),productAddValidator,create)
-=======
-  .put('/editar/:id',upload.single('mainImage') , update)
-  .post('/create',productAddValidator,upload.single('mainImage'),create)
->>>>>>> 3b5c55f7c27b80c465e86e386ebc0c622e96aa1f
   .delete('/delete/:id', remove)
   .get('/category/:id', filterCat) 
 
